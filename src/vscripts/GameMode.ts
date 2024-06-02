01da444d74956ccd7a35a2124e683b1970c3ea20
John Maher <c3283328@uon.edu.au>
2024-06-02 21:24:19 +1000

    Precache all the things :)

--- src/vscripts/GameMode.ts
+++ src/vscripts/GameMode.ts
@@ -31,6 +31,31 @@ declare global {
     }
 }
 
+const getHeroToSoundNameMapping = (heroName: string): string => {
+    switch (heroName) {
+        case "night_stalker":
+            return "nightstalker";
+        case "drow_ranger":
+            return "drowranger";
+        case "shadow_shaman":
+            return "shadowshaman";
+        case "skeleton_king":
+            return "skeletonking";
+        case "crystal_maiden":
+            return "crystalmaiden";
+        case "witch_doctor":
+            return "witchdoctor";
+        case "storm_spirit":
+            return "stormspirit";
+        case "sand_king":
+            return "sandking";
+        case "doom_bringer":
+            return "doombringer";
+        default:
+            return heroName;
+    }
+};
+
 @reloadable
 export class GameMode {
     public static Precache(this: void, context: CScriptPrecacheContext) {
@@ -44,6 +69,34 @@ export class GameMode {
             "soundevents/game_sounds_heroes/game_sounds_meepo.vsndevts",
             context
         );
+
+        const heroList = LoadKeyValues("scripts/npc/hero_list.txt");
+        const heroNames = Object.keys(heroList);
+
+        const particleList = LoadKeyValues(
+            "scripts/particles/hero_particles.txt"
+        );
+        const particleNames = Object.keys(particleList);
+        // Particle Effects
+        for (const key of particleNames) {
+            const keyCopy = key.substring(0, key.length - 2);
+            PrecacheResource(
+                "particle",
+                `particles/units/heroes/${keyCopy}`,
+                context
+            );
+        }
+
+        // Hero Sounds
+        for (const key of heroNames) {
+            const heroName = key.replace("npc_dota_hero_", "");
+            const heroSoundName = getHeroToSoundNameMapping(heroName);
+            PrecacheResource(
+                "soundfile",
+                `soundevents/game_sounds_heroes/game_sounds_${heroSoundName}.vsndevts`,
+                context
+            );
+        }
     }
 
     public static Activate(this: void) {
@@ -121,7 +174,7 @@ export class GameMode {
 
     private onAbilityPickPhaseCompleted(): void {
         GameRules.SetPreGameTime(30);
-        Timers.CreateTimer(30, () => {
+        Timers.CreateTimer(IsInToolsMode() ? 1 : 30, () => {
             GameRules.ForceGameStart();
         });
         const gameModeEntity = GameRules.GetGameModeEntity();
